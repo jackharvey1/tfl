b10e863ce3e9834c79596ac3341d4ef5d667be5a
Jack Harvey <jack.harvey@news.co.uk>
2017-01-27 21:57:28 +0000

    Remove merge test

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -6,33 +6,6 @@ const utils = require('../../src/helpers/utils');
 
 describe('Helper functions', function() {
     describe('of type utility', function() {
-        it('merges arrays of objects', function() {
-            let arr = [{
-                one: {
-                    oneone: 1
-                }
-            }, {
-                one: {
-                    onetwo: 1
-                }
-            }, {
-                two: {
-                    twoone: 2
-                }
-            }];
-
-            arr = utils.mergeObjectArray(arr);
-
-            expect(arr).to.deep.equal({
-                one: {
-                    oneone: 1,
-                    onetwo: 1
-                }, two: {
-                    twoone: 2
-                }
-            });
-        });
-
         it('finds duplicates', function() {
             let arr = [
                 {
