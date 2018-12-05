15e41a404eff174b31c527a8c11caa5b75d5627b
Jack Harvey <jackrrv@gmail.com>
2018-12-05 00:04:49 +0000

    Use s3 backend for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3" {
+        bucket = "jackharvey-terraform-state"
+        key    = "tfl"
+        region = "eu-west-2"
+    }
+}
+
 provider "aws" {
     region  = "eu-west-2"
     profile = "personal"
